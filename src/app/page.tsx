07a20b13b5b2943815fa94f16b260ddb219e0c3b
Prachi Singh <prachi@searchboxed.com>
2024-03-22 02:44:23 +0530

    feat: add navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import Navbar from "@/components/Navbar";
+
 
 export default function Home() {
   return (
     <>
-       <p>Hello</p>
+      <h1>Hello</h1>
+      <Navbar />
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Disclosure } from '@headlessui/react'
 import { ChevronRightIcon } from '@heroicons/react/20/solid'
 import {
